06624d6904b11d5563d524f4f76a2711d4ed977b
Cameron Yule <cameron@cameronyule.com>
2012-12-24 14:41:34 +0000

    Started work on implementing support for skin deformations

--- mesh.py
+++ mesh.py
@@ -129,11 +129,13 @@ def parse_mesh(node, mesh):
 def bake_global_positions(node, control_points):
     time = FbxTime()
     pose = None
-
+        
     if (scene.GetPoseCount() > 0):
         pose = scene.GetPose(0)
 
     global_position = get_global_position(node, time, pose)
+    
+    control_points = parse_skin_deformation(node.GetNodeAttribute(), global_position, time, control_points, pose)
 
     geometry_offset = get_geometry(node)
     global_offset_position = global_position * geometry_offset
@@ -142,6 +144,256 @@ def bake_global_positions(node, control_points):
         control_points[i] = global_offset_position.MultT(control_points[i])
 
     return control_points
+
+
+def parse_skin_deformation(mesh, global_position, time, control_points, pose):
+    shape_count = mesh.GetShapeCount()
+    skin_count = mesh.GetDeformerCount(FbxDeformer.eSkin)
+
+    if (skin_count > 0):
+        # we need to get the number of clusters
+        cluster_count = 0
+
+        for i in range(skin_count):
+            cluster_count += mesh.GetDeformer(i, FbxDeformer.eSkin).GetClusterCount()
+
+        if cluster_count > 0:
+            # Deform the vertex array with the skin deformer.
+            control_points = compute_skin_deformation(global_position, mesh, time, control_points, pose)
+            
+    return control_points
+
+def compute_skin_deformation(global_position, mesh, time, control_points, pose):
+    skin_deformer = mesh.GetDeformer(0, FbxDeformer.eSkin)
+    skinning_type = skin_deformer.GetSkinningType()
+    
+    if (skinning_type == FbxSkin.eLinear or skinning_type == FbxSkin.eRigid):
+        print 'linear or rigid'
+        control_points = compute_linear_deformation(global_position, mesh, time, control_points, pose)
+    # elif (skinning_type == FbxSkin.eDualQuaternion):
+    #     print 'eDualQuaternion'
+    # elif (skinning_type == FbxSkin.eBlend):
+    #     print 'eBlend'
+        
+    return control_points
+    
+    # if(lSkinningType == FbxSkin::eLinear || lSkinningType == FbxSkin::eRigid)
+    # {
+    #     ComputeLinearDeformation(pGlobalPosition, pMesh, pTime, pVertexArray, pPose);
+    # }
+    # else if(lSkinningType == FbxSkin::eDualQuaternion)
+    # {
+    #     ComputeDualQuaternionDeformation(pGlobalPosition, pMesh, pTime, pVertexArray, pPose);
+    # }
+    # else if(lSkinningType == FbxSkin::eBlend)
+    # {
+    #     int lVertexCount = pMesh->GetControlPointsCount();
+    # 
+    #     FbxVector4* lVertexArrayLinear = new FbxVector4[lVertexCount];
+    #     memcpy(lVertexArrayLinear, pMesh->GetControlPoints(), lVertexCount * sizeof(FbxVector4));
+    # 
+    #     FbxVector4* lVertexArrayDQ = new FbxVector4[lVertexCount];
+    #     memcpy(lVertexArrayDQ, pMesh->GetControlPoints(), lVertexCount * sizeof(FbxVector4));
+    # 
+    #     ComputeLinearDeformation(pGlobalPosition, pMesh, pTime, lVertexArrayLinear, pPose);
+    #     ComputeDualQuaternionDeformation(pGlobalPosition, pMesh, pTime, lVertexArrayDQ, pPose);
+    # 
+    #     // To blend the skinning according to the blend weights
+    #     // Final vertex = DQSVertex * blend weight + LinearVertex * (1- blend weight)
+    #     // DQSVertex: vertex that is deformed by dual quaternion skinning method;
+    #     // LinearVertex: vertex that is deformed by classic linear skinning method;
+    #     int lBlendWeightsCount = lSkinDeformer->GetControlPointIndicesCount();
+    #     for(int lBWIndex = 0; lBWIndex<lBlendWeightsCount; ++lBWIndex)
+    #     {
+    #         double lBlendWeight = lSkinDeformer->GetControlPointBlendWeights()[lBWIndex];
+    #         pVertexArray[lBWIndex] = lVertexArrayDQ[lBWIndex] * lBlendWeight + lVertexArrayLinear[lBWIndex] * (1 - lBlendWeight);
+    #     }
+    # }
+
+
+def compute_linear_deformation(global_position, mesh, time, control_points, pose):
+    skin_deformer = mesh.GetDeformer(0, FbxDeformer.eSkin)
+    cluster = skin_deformer.GetCluster(0)
+    cluster_mode = cluster.GetLinkMode()
+    
+    vertex_count = mesh.GetControlPointsCount()
+    cluster_deformation = [FbxAMatrix()] * vertex_count    
+    cluster_weight = [float()] * vertex_count
+
+    # import pdb; pdb.set_trace()
+        
+    if (cluster_mode == FbxCluster.eAdditive):
+        for i in range(vertex_count):
+            cluster_deformation[i].SetIdentity()
+
+    # For all skins and all clusters, accumulate their deformation and weight
+    # on each vertices and store them in cluster_deformation and cluster_weight.
+    skin_count = mesh.GetDeformerCount(FbxDeformer.eSkin)
+    
+    for i in range(skin_count):
+        skin_deformer = mesh.GetDeformer(i, FbxDeformer.eSkin)
+        cluster_count = skin_deformer.GetClusterCount()
+        
+        for j in range(cluster_count):
+            cluster = skin_deformer.GetCluster(j)
+
+            if (not cluster.GetLink()):
+                continue
+
+            vertex_transformation_matrix = FbxAMatrix()
+            vertex_transformation_matrix = compute_cluster_deformation(global_position, mesh, cluster, vertex_transformation_matrix, time, pose)
+            
+            vertex_index_count = cluster.GetControlPointIndicesCount()
+            
+            for k in range(vertex_index_count):
+                index = cluster.GetControlPointIndices()[k]
+
+                # Sometimes, the mesh can have less points than at the time of the skinning
+                # because a smooth operator was active when skinning but has been deactivated during export.
+                if (index >= vertex_count):
+                    continue
+
+                weight = cluster.GetControlPointWeights()[k]
+                
+                if (weight == 0.0):
+                    continue
+                
+                # Compute the influence of the link on the vertex.
+                influence = vertex_transformation_matrix
+                influence = matrix_scale(influence, weight)
+                
+                if (cluster_mode == FbxCluster.eAdditive):
+                    influence = matrix_add_to_diagonal(influence, 1.0 - weight)
+                    cluster_deformation[index] *= influence
+                    # Set the link to 1.0 just to know this vertex is influenced by a link.
+                    cluster_weight[index] = 1.0
+                else:
+                    cluster_deformation[index] = matrix_add(cluster_deformation[index], influence)
+                    cluster_weight[index] += weight
+
+    # Actually deform each vertices here by information stored in lClusterDeformation and lClusterWeight
+    for i in range(vertex_count):
+        source_vertex = control_points[i]
+        destination_vertex = control_points[i]
+        weight = cluster_weight[i]
+
+        # Deform the vertex if there was at least a link with an influence on the vertex,
+        if (weight != 0.0):
+            destination_vertex = cluster_deformation[i].MultT(source_vertex)
+            
+            if (cluster_mode == FbxCluster.eNormalize):
+                # In the normalized link mode, a vertex is always totally influenced by the links. 
+                destination_vertex /= weight
+            elif (cluster_mode == FbxCluster.eTotalOne):
+                # In the total 1 link mode, a vertex can be partially influenced by the links. 
+                source_vertex *= (1.0 - weight)
+                destination_vertex += source_vertex
+                
+    return control_points
+
+
+# Compute the transform matrix that the cluster will transform the vertex.
+def compute_cluster_deformation(global_position, mesh, cluster, vertex_transformation_matrix, time, pose):
+    cluster_mode = cluster.GetLinkMode()
+
+    reference_global_init_position = FbxAMatrix()
+    reference_global_current_position = FbxAMatrix()
+    associate_global_init_position = FbxAMatrix()
+    associate_global_current_position = FbxAMatrix()
+    cluster_global_init_position = FbxAMatrix()
+    cluster_global_current_position = FbxAMatrix()
+
+    reference_geometry = FbxAMatrix()
+    associate_geometry = FbxAMatrix()
+    cluster_geometry = FbxAMatrix()
+
+    cluster_relative_init_position = FbxAMatrix()
+    cluster_relative_current_position_inverse = FbxAMatrix()
+    
+    if (cluster_mode == FbxCluster.eAdditive and cluster.GetAssociateModel()):
+        associate_global_init_position = cluster.GetTransformAssociateModelMatrix(associate_global_init_position)
+        # Geometric transform of the model
+        associate_geometry = get_geometry(cluster.GetAssociateModel())
+        associate_global_init_position *= associate_geometry
+        associate_global_current_position = get_global_position(cluster.GetAssociateModel(), time, pose)
+        
+        reference_global_init_position = cluster.GetTransformMatrix()
+        # Multiply lReferenceGlobalInitPosition by Geometric Transformation
+        reference_geometry = get_geometry(mesh.GetNode())
+        reference_global_init_position *= reference_geometry
+        reference_global_current_position = global_position
+        
+        # Get the link initial global position and the link current global position.
+        cluster_global_init_position = cluster.GetTransformLinkMatrix()
+        # Multiply lClusterGlobalInitPosition by Geometric Transformation
+        cluster_geometry = get_geometry(cluster.GetLink())
+        cluster_global_init_position *= cluster_geometry
+        cluster_global_current_position = get_global_position(cluster.GetLink(), time, pose)
+
+        # Compute the shift of the link relative to the reference.
+        # ModelM-1 * AssoM * AssoGX-1 * LinkGX * LinkM-1*ModelM
+        vertex_transformation_matrix = reference_global_init_position.Inverse() * associate_global_init_position * associate_global_current_position.Inverse() * cluster_global_current_position * cluster_global_init_position.Inverse() * reference_global_init_position
+    else:
+        reference_global_init_position = cluster.GetTransformMatrix(reference_global_init_position)
+        reference_global_current_position = global_position
+        
+        # Multiply lReferenceGlobalInitPosition by Geometric Transformation
+        reference_geometry = get_global_position(mesh.GetNode(), time, pose)
+        reference_global_init_position *= reference_geometry
+        
+        # Get the link initial global position and the link current global position.
+        cluster_global_init_position = cluster.GetTransformLinkMatrix(cluster_global_init_position)
+        cluster_global_current_position = get_global_position(cluster.GetLink(), time, pose)
+        
+        # Compute the initial position of the link relative to the reference.
+        cluster_relative_init_position = cluster_global_init_position.Inverse() * reference_global_init_position
+        
+        # Compute the current position of the link relative to the reference.
+        cluster_relative_current_position_inverse = reference_global_current_position.Inverse() * cluster_global_current_position
+        
+        # Compute the shift of the link relative to the reference.
+        vertex_transformation_matrix = cluster_relative_current_position_inverse * cluster_relative_init_position
+    
+    return vertex_transformation_matrix
+    
+
+def matrix_scale(matrix, value):
+    new_matrix = FbxAMatrix()
+
+    for i in range(4):
+        row = FbxVector4(matrix[i][0] * value, matrix[i][1] * value, matrix[i][2] * value, matrix[i][3] * value)
+        new_matrix.SetRow(i, row)
+
+    return new_matrix
+    
+
+def matrix_add(destination_matrix, source_matrix):
+    new_matrix = FbxAMatrix()
+    
+    for i in range(4):
+        a = destination_matrix[i][0] * source_matrix[i][0]
+        b = destination_matrix[i][1] * source_matrix[i][1]
+        c = destination_matrix[i][2] * source_matrix[i][2]
+        d = destination_matrix[i][3] * source_matrix[i][3]
+
+        new_matrix.SetRow(i, FbxVector4(a, b, c, d))
+
+    return new_matrix
+
+
+# Add a value to all the elements in the diagonal of the matrix.
+def matrix_add_to_diagonal(matrix, value):
+    new_matrix = FbxAMatrix()
+
+    for i in range(4):
+        a = matrix[i][0] + (value if i == 0 else 0)
+        b = matrix[i][1] + (value if i == 1 else 0)
+        c = matrix[i][2] + (value if i == 2 else 0)
+        d = matrix[i][3] + (value if i == 3 else 0)
+
+        new_matrix.SetRow(i, FbxVector4(a, b, c, d))
+
+    return new_matrix
 
 
 def get_global_position(node, time, pose):
