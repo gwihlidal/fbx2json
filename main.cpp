91e5c841323c26f8cfa58fd8fd6fda36ee92b3c5
Cameron Yule <cameron@cameronyule.com>
2013-02-10 19:20:12 +0000

    Removing dependency on boost::program_options for parsing arguments, which was too heavyweight for what I need.

--- main.cpp
+++ main.cpp
@@ -6,81 +6,28 @@
 //  Copyright (c) 2013 Cameron Yule. All rights reserved.
 //
 
-#include <boost/program_options.hpp>
-#include <boost/filesystem.hpp>
-namespace po = boost::program_options;
-
 #include <iostream>
 #include <string>
 using namespace std;
 
-const std::string VERSION = "1.0";
+#define FBXSDK_NEW_API true
+#include <fbxsdk.h>
 
-namespace
+void usage(char* prog)
 {
-    const size_t ERROR_IN_COMMAND_LINE = 1;
-    const size_t SUCCESS = 0;
-    const size_t ERROR_UNHANDLED_EXCEPTION = 2;    
+    std::cerr << prog << ": missing arguments" << std::endl;
+	fprintf(stderr, "\nUSAGE: %s [FBX inputFile] [JSON outputFile] \n", prog);
 }
 
 int main(int argc, char** argv)
 {
-    try
-    {
-        std::string input;
-        std::string output;
-        
-        std::string appName = boost::filesystem::basename(argv[0]);
-        std::string appVersion = appName + " " + VERSION;
-        
-        po::options_description desc("Options");
-        desc.add_options()
-        ("help", "Print help messages")
-        ("input", po::value<std::string>(&input)->required(), "Input FBX file")
-        ("output", po::value<std::string>(&output)->required(), "Output JSON file");
-        
-        po::positional_options_description positionalOptions;
-        positionalOptions.add("input", 1);
-        positionalOptions.add("output", 1);
-        
-        po::variables_map vm;
-
-        try
-        {
-            po::store(po::command_line_parser(argc, argv).options(desc)
-                      .positional(positionalOptions).run(),
-                      vm);
-            
-            if ( vm.count("help")  )
-            {
-                std::cout << appVersion << std::endl << std::endl
-                << "USAGE: " << appName << " input.fbx output.json" << std::endl << std::endl
-                << desc << std::endl;
-                return SUCCESS;
-            }
-            
-            po::notify(vm);
-        }
-        catch(po::error& e)
-        {
-            std::cerr << appVersion << std::endl << std::endl;
-            std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
-            std::cerr << "USAGE: " << appName << " input.fbx output.json" << std::endl << std::endl;
-            std::cerr << desc << std::endl;
-            return ERROR_IN_COMMAND_LINE;
-        }
-        
-        std::cout << "Required Positional, input: " << input
-        << " output: " << output << std::endl;
-        
-        // application code here //
-        
+    if (argc < 3) {
+		usage(argv[0]);
+		return EXIT_FAILURE;
     }
-    catch(std::exception& e)
-    {
-        std::cerr << "Unhandled Exception " << e.what() << ", application will now exit" << std::endl;
-        return ERROR_UNHANDLED_EXCEPTION;
-    } 
     
-    return SUCCESS;
+    std::string inputFile = argv[1];
+    std::string outputFile = argv[2];
+    
+    return EXIT_SUCCESS;
 }
